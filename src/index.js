2695f817b1ea2e59b6f48c99121a7acb74044343
LilithVinter <108224035+LilithVinter@users.noreply.github.com>
2024-06-14 22:01:26 +0200

    added AI for name generator (api js)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generator(event) {
-  event.preventDefault();
-
+function displayName(response) {
   new Typewriter("#nameList", {
-    strings: [`<ul> <li> name 1</li> <li> name 1</li><li> name 1</li></ul>`],
+    strings: response.data.answer,
     autoStart: true,
     delay: 5,
     cursor: "",
   });
+}
+
+function generator(event) {
+  event.preventDefault();
+
+  let instructions = document.querySelector("#user-instructions");
+
+  let apiKey = `67160eaaec4o69a29b0ff296te075931`;
+  let prompt = `user instructions: generate 5 names related to ${instructions.value}`;
+  let context =
+    "you are a name generator that only provides response in list format within <ul> with no list style following the user instructions";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(`${prompt}`);
+
+  axios.get(apiURL).then(displayName);
 }
 
 let nameInput = document.querySelector("#name-generator-form");
